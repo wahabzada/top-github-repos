4ac3c01f0faceab1ed2c331c3b933debed7869cf
Farzad Wahabzada <imfrzd@gmail.com>
2023-02-20 22:28:52 +0000

    added repos folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import { useEffect } from "react"
 import { useRouter } from "next/router"
 import { DefaultLayout } from "layouts/DefaultLayout"
 import { useReposAction } from "state/repos/useReposAction"
-import { RepoLister } from "components/stateful/RepoLister"
-import { RepoSearch } from "components/stateful/RepoSearch"
+import { RepoLister } from "components/stateful/repos/RepoLister"
+import { RepoSearch } from "components/stateful/repos/RepoSearch"
 
 export default function Home() {
   const router = useRouter()
