290c5f522b81296c32c776d66ac73d17de34c4a5
Farzad Wahabzada <imfrzd@gmail.com>
2023-02-21 16:30:18 +0000

    lifecycle hook moved to the lister

--- components/stateful/repos/RepoLister.tsx
+++ components/stateful/repos/RepoLister.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react"
 import { useReposState } from "state/repos/useReposState"
 import { RepoCard } from "components/stateless/card/RepoCard"
 import { RepoFilters } from "./RepoFilters"
+import { useReposAction } from "state/repos/useReposAction"
+import { useRouter } from "next/router"
 
 export const RepoLister: React.FC = () => {
+  const router = useRouter()
+  const { language } = router.query
+
   const { loading, error, totalRepos, repos } = useReposState()
+  const { fetchRepos, resetRepos } = useReposAction()
+
+  useEffect(() => {
+    if (language) {
+      fetchRepos(language as string)
+    }
+
+    return () => resetRepos()
+  }, [language])
 
   if (loading || error) {
     return null
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,8 @@
-import { useEffect } from "react"
-import { useRouter } from "next/router"
 import { DefaultLayout } from "layouts/DefaultLayout"
-import { useReposAction } from "state/repos/useReposAction"
 import { RepoLister } from "components/stateful/repos/RepoLister"
 import { RepoSearch } from "components/stateful/repos/RepoSearch"
 
 export default function Home() {
-  const router = useRouter()
-  const { language } = router.query
-
-  const { fetchRepos, resetRepos } = useReposAction()
-
-  useEffect(() => {
-    if (language) {
-      fetchRepos(language as string)
-    }
-
-    return () => resetRepos()
-  }, [language])
 
   return (
     <DefaultLayout>
